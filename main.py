ec99450a6eacd409459d9df5c3c1ab431716b6ee
mcgillij <mcgillivray.jason@gmail.com>
2025-03-18 19:27:57 -0300

    adds favicon / static files

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request, Header, Form
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.encoders import jsonable_encoder
+from fastapi.staticfiles import StaticFiles
 from typing import Annotated, Union, List
 import uuid  # For unique IDs
 from game import Game, Platforms, Genres
@@ -17,9 +18,9 @@ logger.setLevel(logging.DEBUG)
 games = []
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
-
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
     return templates.TemplateResponse(request=request, name="index.html")
